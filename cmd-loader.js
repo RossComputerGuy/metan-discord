68c16b11829e3004e5b555a1f8d81d4e77dcccb8
SpaceboyRoss01 <spaceboyross@yandex.com>
2019-01-14 21:36:30 -0800

    Added command execution and conforms to eslint

--- cmd-loader.js
+++ cmd-loader.js
@@ -1,8 +1,9 @@
 const fs = require('fs');
 const path = require('path');
 
-module.exports = (client,argv,message) => {
+module.exports = argv => {
 	let cmd = argv['_'].shift();
-	if (require.cache[require.resolve(path.join(__dirname,'commands',cmd))]) delete require.cache[require.resolve(path.join(__dirname,'commands',cmd))];
-	return require(path.join(__dirname,'commands',cmd));
+	let loc = require.resolve(path.join(__dirname, 'commands', cmd));
+	if (require.cache[loc]) delete require.cache[loc];
+	return require(loc);
 };
--- index.js
+++ index.js
@@ -1,2 +1,20 @@
+const commandLoader = require('./cmd-loader');
 const Discord = require('discord.js');
+const {readConfig} = require('./utils');
+const yargsParser = require('yargs-parser');
+
 const client = new Discord.Client();
+
+client.on('message', message => {
+	if (message.content.startsWith(readConfig().prefix) && !message.author.bot) {
+		let args = yargsParser(message.content);
+		if(args['_'].length < 1) return;
+		try {
+			commandLoader(args).execute(client,message);
+		} catch(ex) {
+			message.reply('The command failed to execute! Dumping error:\n```' + ex.stack + '\n```');
+		}
+	}
+});
+
+client.login(readConfig().login.token);
